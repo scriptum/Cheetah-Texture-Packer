c09882f38ab5aa90c3063633ef597aeae245acc7
Sergey <non_2001@mail.ru>
2013-08-16 11:27:38 +0400

    - algorithm improvement  - new command line features     --disable-recursion  //just one folder not subfolder     --disable-not-square //only square atlases     -autosize-threshold value //use autosize algorithm only if fill rate in percents < value     -min-texture-size WidthxHeight //use autosize algorithm only if last texture size > min texture size     -crop-threshold value //crop alpha threshold 0-255     -sortorder value //sort order line in interface new sort order algorithm MAX

--- src/imagepacker.h
+++ src/imagepacker.h
@@ -4,6 +4,8 @@
 #include <QObject>
 #include <QImage>
 
+class MaxRects;
+
 struct packedImage
 {
     QImage img;
@@ -23,6 +25,7 @@ struct inputImage
     QPoint pos;
     QSize size, sizeCurrent;
     QRect crop;
+    QString path;
 
     bool cropped, rotated;
 };
@@ -35,33 +38,54 @@ class ImagePacker : public QObject
 {
 private:
     int prevSortOrder;
+    void internalPack(int heur, int w, int h);
+
+    void SortImages( int w, int h );
+
 public:
     QList<inputImage> images;
     QList<QSize> bins;
     ImagePacker();
     bool compareImages(QImage* img1, QImage* img2, int* i, int *j);
-    void pack(int heur, int w, int h = 512);
-    QRect crop(QImage*);
+    void pack(int heur, int w, int h);
+
+    unsigned AddImgesToBins( int heur, int w, int h);
+
+    void CropLastImage(int heur, int w, int h, bool wh);
+    void DivideLastImage(int heur, int w, int h, bool wh);
+
+    void UpdateCrop();
+
+    float GetFillRate();
+
+    void ClearBin( int binIndex );
+
+    int FillBin( int heur, int w, int h, int binIndex);
+
+    QRect crop(const QImage &img);
     void sort();
-    void addItem(QImage img, void * data);
+    void addItem(const QImage &img, void * data, QString path);
     void addItem(QString path, void * data);
     const inputImage *find(void * data);
     void removeId(void*);
     void realculateDuplicates();
     void clear();
     int compare;
-    quint64 area, summArea, neededArea;
+    quint64 area, neededArea;
     int missingImages;
     int mergedImages;
-    bool ltr, merge, mergeBF;
-    int trim;
+    bool ltr, merge, square, autosize, mergeBF;
+    int cropThreshold;
     border_t border;
     int rotate;
     int sortOrder;
+    int minFillRate;
+    int minTextureSizeX;
+    int minTextureSizeY;
     enum {GUILLOTINE, MAXRECTS}; //method
     enum {NONE, TL, BAF, BSSF, BLSF, MINW, MINH, HEURISTIC_NUM}; //heuristic
-    enum {SORT_NONE, WIDTH, HEIGHT, SORT_NUM}; //sort
-    enum {NEVER, H2_WIDTH_H, WIDTH_GREATHER_HEIGHT, WIDTH_GREATHER_2HEIGHT, W2_HEIGHT_W, HEIGHT_GREATHER_WIDTH, HEIGHT_GREATHER_2WIDTH, ROTATION_NUM}; //rotation
+    enum {SORT_NONE, WIDTH, HEIGHT, SORT_AREA, SORT_MAX, SORT_NUM}; //sort
+    enum {NEVER, ONLY_WHEN_NEEDED, H2_WIDTH_H, WIDTH_GREATHER_HEIGHT, WIDTH_GREATHER_2HEIGHT, W2_HEIGHT_W, HEIGHT_GREATHER_WIDTH, HEIGHT_GREATHER_2WIDTH, ROTATION_NUM}; //rotation
 };
 
 
